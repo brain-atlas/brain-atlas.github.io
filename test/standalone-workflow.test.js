251831aea7c16e744ea45e296e776156bb8909ba
Steve Hay <github@stevenhay.com>
2026-08-10 11:44:36 -0400

    test: refresh checkout action pin (#34)

--- test/standalone-workflow.test.js
+++ test/standalone-workflow.test.js
@@ -6,7 +6,7 @@ import { parse } from 'yaml';
 
 const WORKFLOW_PATH = new URL('../.github/workflows/standalone-binaries.yml', import.meta.url);
 const ACTIONS = {
-  checkout: 'actions/checkout@9c091bb21b7c1c1d1991bb908d89e4e9dddfe3e0',
+  checkout: 'actions/checkout@3d3c42e5aac5ba805825da76410c181273ba90b1',
   setupNode: 'actions/setup-node@820762786026740c76f36085b0efc47a31fe5020',
   setupGo: 'actions/setup-go@b7ad1dad31e06c5925ef5d2fc7ad053ef454303e',
   upload: 'actions/upload-artifact@043fb46d1a93c77aae656e7c1c64a875d1fc6a0a',
